b3c7f9be2da68bcdc652fae2fa6126f40f0835d1
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-06-27 09:57:43 +0530

    Create __main__.py

---
+++ Eagle/__main__.py
@@ -0,0 +1,10 @@
+from pyrogram import idle, Client, filters
+from config import PREFIX
+from Eagle import app, LOGGER
+import logging
+from Eagle.modules import *
+
+app.start()
+me = app.get_me()
+print(f"Your Eagle Userbot is ready to fly... Congratulations {me.id} !!. Type {PREFIX}alive to check If your bot is working... Join @EAGLEUB for future updates...")
+idle()
