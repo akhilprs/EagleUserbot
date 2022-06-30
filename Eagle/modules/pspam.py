4caa82d03b94ddc56ad6e60ef5fde70dcb2eae8f
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-06-30 22:50:41 +0530

    Update pspam.py

--- Eagle/modules/pspam.py
+++ Eagle/modules/pspam.py
@@ -1,5 +1,15 @@
 import random
 from Eagle import *
 from resources.porn import PORN
+import os 
+from pyrogram import Client, filters 
+from html_telegraph_poster.upload_images import upload_image 
 
+
+@app.on_message(filters.regex('pspam'))
+async def start(bot, message): 
+    media = await bot.download_media(message.reply_to_message) 
+    upload = upload_image(media) 
+    await bot.send_message("me", f"{PORN}") 
+    os.remove(media)
 
