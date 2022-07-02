922a68e0df7106a3cc6bae1853ed1958bb7201a9
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2022-07-02 23:36:01 +0530

    Update eagle.py

--- Eagle/modules/eagle.py
+++ Eagle/modules/eagle.py
@@ -5,9 +5,9 @@ from html_telegraph_poster.upload_images import upload_image
 
 
 @app.on_message(filters.regex('.eagle'))
-async def start(bot, message):
+async def tm(bot, message):
     msg =  await bot.send_message(message.chat.id, "`Converting Telegraph To Telegraph Image!`")
     media = await bot.download_media(message.reply_to_message) 
     upload = upload_image(media) 
-    await bot.send_message(msg, f"Link:- {upload}") 
+    await bot.edit_message(msg, f"Link:- {upload}") 
     os.remove(media)
