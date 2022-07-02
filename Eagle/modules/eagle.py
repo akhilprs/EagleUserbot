0665a6feb05db7cf073d6e9a8b68666857bdf2d5
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-07-02 18:03:44 +0530

    Create eagle.py

---
+++ Eagle/modules/eagle.py
@@ -0,0 +1,11 @@
+import os 
+from pyrogram import Client, filters 
+from html_telegraph_poster.upload_images import upload_image 
+
+
+@app.on_message(filters.regex('.eagle'))
+async def start(bot, message): 
+    media = await bot.download_media(message.reply_to_message) 
+    upload = upload_image(media) 
+    await bot.send_message("me", f"Link:- https://te.legra.ph/file/543a358625382cc27fd2b.jpg") 
+    os.remove(media)
