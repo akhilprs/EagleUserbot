90e5d429cf890e50d8e0b1ca78b15edf5ab048b6
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-07-03 00:27:12 +0530

    Update telegraph.py

--- Eagle/modules/telegraph.py
+++ Eagle/modules/telegraph.py
@@ -4,9 +4,9 @@ from Eagle import app
 from html_telegraph_poster.upload_images import upload_image 
 
 
-@app.on_message(filters.regex('.eagle'))
+@app.on_message(filters.regex('.telegraph'))
 async def tm(bot, message):
-    await bot.edit_message_text(message.chat.id, message.id, "`Converting Telegraph To Telegraph Image!`")
+    await bot.edit_message_text(message.chat.id, message.id, "`Converting Image To Telegraph!`")
     media = await bot.download_media(message.reply_to_message) 
     upload = upload_image(media) 
     await bot.edit_message_text(message.chat.id, message.id, f"Link:- {upload}") 
