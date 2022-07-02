90482977e4ff39eb4bcb1f2aff162105668f4ce1
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2022-07-02 16:17:25 +0530

    MODULE UPDATED BY H1M4N5HU0P

--- Eagle/__main__.py
+++ Eagle/__main__.py
@@ -1,9 +1,34 @@
 from pyrogram import idle, Client, filters
 from config import PREFIX
-from Eagle import app, LOGGER
+from Eagle import *
 import logging
-from Eagle.modules import *
 import uvloop
+import sys
+import glob
+from pathlib import Path
+
+
+# PLUGIN LOAD SUPPORT BY @H1M4N5HU0P
+
+def load_plugs(plugname)
+    modules = Path(f"Eagle/modules/{plugname}.py")
+    myfiles = f"Eagle.modules.{plugname}"
+    spec = importlib.util.spec_from_file_location(myfiles, modules)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugname)
+    spec.loader exec_module(load)
+    sys.modules["Eagle.modules." + plugname] = load
+    print("Eagle Userbot - Successfully Imported " plugname)
+
+if __name__ == "__main__":
+    modules = "Eagle/modules/*.py"
+    plugins = glob.glob(modules)
+    for myfiles in plugins:
+        with open(myfiles) as f:
+            module = Path(f.name)
+            plugname = module.stem
+            load_plugs(plugname.replace(".py", ""))
+
 
 uvloop.install() # speedup
 
